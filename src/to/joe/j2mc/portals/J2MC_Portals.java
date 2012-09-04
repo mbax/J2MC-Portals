202204015970078a45e50d8f81f940659ddab9ed
mbax <matt@phozop.net>
2012-09-04 12:03:57 -0400

    Format

--- src/to/joe/j2mc/portals/J2MC_Portals.java
+++ src/to/joe/j2mc/portals/J2MC_Portals.java
@@ -18,36 +18,36 @@ public class J2MC_Portals extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        if(!(new File(this.getDataFolder(),"config.yml")).exists()){
+        if (!(new File(this.getDataFolder(), "config.yml")).exists()) {
             this.saveDefaultConfig();
         }
-        
+
         this.getServer().getPluginManager().registerEvents(this, this);
         this.loadPortalAreas();
     }
-    
+
     public void loadPortalAreas() {
         this.reloadConfig();
-        for(String area : this.getConfig().getStringList("portals")) {
+        for (String area : this.getConfig().getStringList("portals")) {
             String path = /*"portals." + */area;
             int x = this.getConfig().getInt(path + ".x");
             int y = this.getConfig().getInt(path + ".y");
             int z = this.getConfig().getInt(path + ".z");
             Location upperLeft = new Location(this.getServer().getWorlds().get(0), x, y, z);
-            
+
             HashSet<Location> locations = new HashSet<Location>();
             List<String> shape = this.getConfig().getStringList(path + ".shape");
             //int height = shape.size();
             int width = shape.get(0).length();
             int curZ = z;
-            for(String line : shape) {
-                if(line.length() != width) {
+            for (String line : shape) {
+                if (line.length() != width) {
                     this.getLogger().info("YOUR CONFIG IS BAD AND YOU SHOULD FEEL BAD. Shutting down plugin.");
                     this.getPluginLoader().disablePlugin(this);
                     return;
                 }
-                for(int i = 0; i < width; i++) {
-                    if(line.charAt(i) == 'X') {
+                for (int i = 0; i < width; i++) {
+                    if (line.charAt(i) == 'X') {
                         Location loc = new Location(this.getServer().getWorlds().get(0), x + i, y, curZ);
                         locations.add(loc);
                         loc.getBlock().setType(Material.BEDROCK); //debug
@@ -56,24 +56,24 @@ public class J2MC_Portals extends JavaPlugin implements Listener {
                 }
                 curZ++;
             }
-            
+
             String destination = area;
             String perm = this.getConfig().getString(path + ".permission");
-            if(perm.equals("j2mc.portals.everyone")) {
-                this.getServer().getPluginManager().addPermission(new Permission("perm",PermissionDefault.OP));
+            if (perm.equals("j2mc.portals.everyone")) {
+                this.getServer().getPluginManager().addPermission(new Permission("perm", PermissionDefault.OP));
             }
-            
+
             this.portalAreas.add(new PortalArea(locations, destination, perm, upperLeft));
         }
     }
-    
+
     public PortalArea getPortalForPlayer(Player player) {
-        for(PortalArea area : this.portalAreas){
-            if(area.isPlayerInPortal(player)){
+        for (PortalArea area : this.portalAreas) {
+            if (area.isPlayerInPortal(player)) {
                 return area;
             }
         }
         return null;
     }
-    
+
 }
--- src/to/joe/j2mc/portals/PortalArea.java
+++ src/to/joe/j2mc/portals/PortalArea.java
@@ -6,40 +6,40 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public class PortalArea {
-    
+
     private HashSet<Location> locations;
     private String destination;
     private String permission;
-    
+
     public PortalArea(HashSet<Location> locations, String destination, String permission, Location upperLeft) {
         this.locations = locations;
         this.destination = destination;
         this.permission = permission;
     }
-    
+
     public HashSet<Location> getLocations() {
         return this.locations;
     }
-    
+
     public boolean isLocationInPortal(Location loc) {
-        return locations.contains(new Location(loc.getWorld(), loc.getBlockX(),loc.getBlockY(), loc.getBlockZ()));
+        return locations.contains(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
     }
-    
+
     public boolean isPlayerInPortal(Player player) {
         Location loc = player.getLocation();
-        Location test = new Location(loc.getWorld(), loc.getBlockX(),loc.getBlockY(), loc.getBlockZ());
-        if(locations.contains(test)){
+        Location test = new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+        if (locations.contains(test)) {
             return true;
         }
         test.add(0, 1, 0);
         return locations.contains(test);
     }
-    
-    public String getDestination(){
+
+    public String getDestination() {
         return this.destination;
     }
-    
-    public String getPermission(){
+
+    public String getPermission() {
         return this.permission;
     }
 
